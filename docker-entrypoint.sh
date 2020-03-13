4a59558450be34e645f4445a5249678585c8093e
Ash Robbins <ash.robbins@pulselive.com>
2020-03-13 11:06:55 +0000

    Trying to fix build

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z $PLMM_API ] || [ -z $FOOTBALL_API ]; then
-  echo "The PLMM_API and FOOTBALL_API environment variables must be set!"
-  exit 1
-fi
-
-sed -i "s,https://pl-mm.dev.platform.pulselive.com,${PLMM_API}," /usr/share/nginx/html/scripts/bundle.js
-sed -i "s,https://api.dev.platform.pulselive.com/football,${FOOTBALL_API}," /usr/share/nginx/html/scripts/bundle.js
-nginx -g "daemon off;"
